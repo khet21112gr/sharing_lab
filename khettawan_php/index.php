4c39607114cba80d28f4bdf605c09e9334681809
Duck <170712975+khet21112gr@users.noreply.github.com>
2025-03-17 12:02:02 +0700

    Add files via upload

---
+++ khettawan_php/index.php
@@ -0,0 +1,16 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>login</title>
+</head>
+<body>
+    <form action="./auth.php" method="get">
+        <input type="text" name="username" placeholder="username" required>
+        <input type="password" name="password" placeholder="password" required>
+        <input type="submit" value="Login">
+    </form>
+</body>
+</html>
+
